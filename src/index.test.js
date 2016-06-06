d9010b2f30bbfcbc7d1f54e2a72379f003e5a271
Ruzo <richard@evigy.com>
2016-06-05 20:24:58 -0400

    Main dev setup done.

---
+++ src/index.test.js
@@ -0,0 +1,7 @@
+import expect from 'expect';
+
+describe('Very first test', () => {
+  it('Should pass', () => {
+    expect(true).toEqual(true);
+  });
+});
---
+++ tools/startMessage.js
@@ -0,0 +1,4 @@
+import colors from 'colors';
+
+/* eslint-disable no-console */
+console.log('Running app in developer mode'.green);
